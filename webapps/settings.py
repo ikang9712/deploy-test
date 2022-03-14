cb961ee1f2dde167e63fc4dab2f72fd82919b040
Inho Kang <inhokang@Inhos-MacBook-Pro.local>
2022-03-13 22:07:16 -0400

    heroku import complete

--- webapps/settings.py
+++ webapps/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from django.conf.urls.static import static
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,3 +128,5 @@ LOGIN_URL = '/socialnetwork/login'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
